cbd3c17b570d82e6a340c768c1e81b7ffbfeed32
hasibislam123 <hasib64dj@gmail.com>
2025-08-01 16:16:45 +0600

    aded the new code

---
+++ case.js
@@ -0,0 +1,25 @@
+const school = 'RAJ Uttora Model School';
+
+console.log(school);
+console.log(school.toLowerCase());
+console.log(school.toUpperCase());
+
+// console.log(school);
+//uppercase: ABCD EFG
+// lowercase: abcd efg
+
+
+const subject = 'Chemistry';
+const book = 'chemisTrY';
+
+
+if (subject.toLowerCase() === book.toLowerCase()){
+    console.log('I am reading book aibar porikhay fatay falbo');
+}
+else {
+    console.log('ami pirikhay khube kharap korbo . kichu pari na hasib ar moto. huday huday prishta ultai ar vat khawar jonno');
+}
+
+
+const drink = 'water' ;
+console.log(drink);
--- string.js
+++ string.js
@@ -1,3 +1,34 @@
 const country = 'Bangladesh';
 const division = "New khale";
 const district = `B-Baria`;
+const thana = new String('demra')
+
+
+// console.log (typeof country);
+// console.log(typeof division);
+// console.log (typeof district);
+// console.log(typeof thana);
+console.log (thana);
+
+
+
+
+const numbers = [ 47, 84, 90, 20, 38]; 
+console.log (numbers)
+// console.log(numbers.length);
+// console.log(numbers[4])
+numbers[1] = 1000;
+console.log(numbers)
+
+
+// string is immutable  --> not changeable
+// array is mutable --> you can change  the array elements
+
+
+const capital = 'dha k a';
+// console.log (capital.length);
+// console.log(capital[4]);
+console.log(capital);
+capital[4] = 'n';
+capital[6] = 'm...';
+console.log(capital);
